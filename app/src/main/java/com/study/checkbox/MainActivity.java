4f1d9a9081c7a8e1d52b56d086deb1918fd857da
anabibelforscher <anaolivercontact@gmail.com>
2021-03-18 16:20:09 -0300

    RadioButton added

--- app/src/main/java/com/study/checkbox/MainActivity.java
+++ app/src/main/java/com/study/checkbox/MainActivity.java
@@ -6,30 +6,57 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox mobile, web, desktop, iot;
     private TextView textoresultado;
+    private RadioButton genderFemale , genderMale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { // Método chamado antes da interface ser carregada
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textoresultado = findViewById(R.id.textResultado);
+
+
+        //Checkbox
         mobile = findViewById(R.id.checkMobile);
         web = findViewById(R.id.checkWeb);
         desktop = findViewById(R.id.checkDesktop);
         iot = findViewById(R.id.checkIOT);
-        textoresultado = findViewById(R.id.textResultado);
+        //Radiobutton
+        genderFemale = findViewById(R.id.radioButtonFemale);
+        genderMale = findViewById(R.id.radioButtonMale);
+
 
 
     }
 
 
     public void enviar (View view){
+        radioButton();
         checkbox();
+    }
+
+    public void radioButton (){
+        String message = null;
+        if (genderFemale.isChecked()){
+            message ="Be brave! Code like a girl! ";
+
+        }
+        if (genderMale.isChecked()){
+            message ="You got it dude!";
+
+        }
+
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        toast.show();
+
     }
 
     private void checkbox() {
